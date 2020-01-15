3e04e568cb9d89dd68db1e93c4ef34e963aa9a6f
ikaddoura <kaddoura@vsp.tu-berlin.de>
2020-01-15 11:28:34 +0100

    finalize run classes

--- src/main/java/org/matsim/run/berlin/RunOptDrtOpenBerlinScenarioWithSpeedUp.java
+++ src/main/java/org/matsim/run/berlin/RunOptDrtOpenBerlinScenarioWithSpeedUp.java
@@ -6,6 +6,8 @@ import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
+import org.matsim.drtSpeedUp.DrtSpeedUpConfigGroup;
+import org.matsim.drtSpeedUp.DrtSpeedUpModule;
 import org.matsim.optDRT.OptDrt;
 import org.matsim.optDRT.OptDrtConfigGroup;
 import org.matsim.run.RunBerlinScenario;
@@ -15,8 +17,8 @@ import org.matsim.run.drt.RunDrtOpenBerlinScenario;
  *
  * @author ikaddoura
  */
-public class RunOptDrtOpenBerlinScenario {
-    private static final Logger log = Logger.getLogger(RunOptDrtOpenBerlinScenario.class);
+public class RunOptDrtOpenBerlinScenarioWithSpeedUp {
+    private static final Logger log = Logger.getLogger(RunOptDrtOpenBerlinScenarioWithSpeedUp.class);
     
     public static void main(String[] args) {
     	
@@ -28,21 +30,24 @@ public class RunOptDrtOpenBerlinScenario {
 			args = new String[] {"scenarios/berlin-v5.5-1pct/input/drt/berlin-drt-v5.5-1pct.config.xml"}  ;
 		}
 		
-		Controler controler = new RunOptDrtOpenBerlinScenario().prepareControler(args);
+		Controler controler = new RunOptDrtOpenBerlinScenarioWithSpeedUp().prepareControler(args);
 		controler.run();
 		RunBerlinScenario.runAnalysis(controler);
     }
 
     public Controler prepareControler(String[] args) {
     	
-    	Config config = RunDrtOpenBerlinScenario.prepareConfig(args, new OptDrtConfigGroup());
-    	
+    	Config config = RunDrtOpenBerlinScenario.prepareConfig(args, new OptDrtConfigGroup(), new DrtSpeedUpConfigGroup());
+		DrtSpeedUpModule.adjustConfig(config);
+
     	Scenario scenario = RunDrtOpenBerlinScenario.prepareScenario(config);
     	for( Person person : scenario.getPopulation().getPersons().values() ){
 			person.getPlans().removeIf( (plan) -> plan!=person.getSelectedPlan() ) ;
 		}
     	
     	Controler controler = RunDrtOpenBerlinScenario.prepareControler(scenario);
+    	
+		controler.addOverridingModule(new DrtSpeedUpModule());
     	
         OptDrtConfigGroup optDrtConfigGroup = ConfigUtils.addOrGetModule(config, OptDrtConfigGroup.class);
     	OptDrt.addAsOverridingModule(controler, optDrtConfigGroup);
